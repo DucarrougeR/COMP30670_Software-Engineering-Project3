108d6ebbbf8c5e14581410f7a3618bbfc039b31e
Romain <romainducarrouge@gmail.com>
2016-02-25 14:46:52 +0000

    initial set up of project

---
+++ main.py
@@ -0,0 +1,25 @@
+import optparse
+import sys
+from src.seater import Seater
+
+
+def main(filename=None):
+    # here we create the seater object
+    seater = Seater(1000)
+    
+    # and we read the file line by line, feeding each line to the Seater
+    with open(filename, 'r') as f:
+        for line in f:
+            seater.seat(line.strip())
+            
+    print 'number of occupied seats', seater.number_occupied()
+    return
+
+if __name__ == '__main__':
+    parser = optparse.OptionParser()
+    parser.add_option('-i', dest="filename", default='data/input_assign3.txt')
+    options, remainder = parser.parse_args()
+    
+    sys.exit(main(filename=options.filename))
+
+    
